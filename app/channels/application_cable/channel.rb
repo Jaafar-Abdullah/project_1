42a793794f17608a67ad4114ddfaf86f323e8107
Jafar Abdullah <jafar@Jafars-MacBook-Air.local>
2018-11-11 14:26:55 +0300

    befor heroku

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,13 +1,13 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
 
-  class ChatChannel < ApplicationCable::Channel
-    def subscribed
-    end
-  end
+  # class ChatChannel < ApplicationCable::Channel
+  #   def subscribed
+  #   end
+  # end
  
 
-  class AppearanceChannel < ApplicationCable::Channel
-  end
-  end
+  # class AppearanceChannel < ApplicationCable::Channel
+  # end
+  # end
 end
